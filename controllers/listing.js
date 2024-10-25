32fdeb400ed989e9f0e19212b4f6953a8a3774fb
saathvik-sheerla <saathvik.sheerla@gmail.com>
2024-10-25 17:36:10 +0530

    deployment ready commit v1.0 basic

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,7 +1,4 @@
 const Listing = require('../models/listing.js');
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapToken = process.env.MAP_TOKEN;
-const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req,res)=>{
         const alllistings = await Listing.find({});
