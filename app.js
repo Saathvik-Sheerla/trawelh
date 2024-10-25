2d5e7bdf20c2dbf90cf2ac9472c5d4adff875d27
saathvik-sheerla <saathvik.sheerla@gmail.com>
2024-10-25 16:48:57 +0530

    production ready files version:1.0 basic

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require('ejs-mate');
 const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
@@ -19,7 +20,7 @@ const expressError = require('./utils/expressError.js')
 
 
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect(process.env.ATLASDB_URL);
 }
 main().then(()=>{
     console.log("database connected DB");
@@ -47,20 +48,33 @@ const listingsRouter = require('./routes/listing.js');
 const reviewsRouter = require('./routes/review.js');
 const userRouter = require('./routes/user.js');
 
+const store = MongoStore.create({
+    mongoUrl: process.env.ATLASDB_URL,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 18 * 60 * 60,
+});
+
 
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        expires: Date.now() * 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
+        expires: Date.now() * 18 * 60 * 60 * 1000,
+        maxAge: 18 * 60 * 60 * 1000,
         httpOnly: true,
     },
 }
 
+store.on('error', (err)=>{
+    console.log('ERROR in MONGO-SESSION STORE => ',err);
+});
+
 app.get('/',(req,res)=>{
-    res.send("Home page");
+    res.redirect('/listings');
 });
 
 app.use(session(sessionOptions));
